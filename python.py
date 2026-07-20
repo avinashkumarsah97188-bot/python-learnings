2409dd4207e0dddd78cc2240a1567eb1d1477dca
Avinash kumar Sah <avinashkumarsah97188@gmail.com>
2026-07-20 23:39:11 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,12 @@
+# Program to display student information using formatted output
+
+name = "Avinash kumar sah"
+age = 23
+course = "Python Programming"
+address = "Delhi, India"
+
+print("------ Student Information ------")
+print("Name    : {}".format(name))
+print("Age     : {}".format(age))
+print("Course  : {}".format(course))
+print("Address : {}".format(address))
