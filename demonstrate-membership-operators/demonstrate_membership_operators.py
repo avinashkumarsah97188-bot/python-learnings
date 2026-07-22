16578819b8fa61a7211941e1410ef3c9b1064f2e
Avinash kumar Sah <avinashkumarsah97188@gmail.com>
2026-07-22 23:48:11 +0530

    demonstrate membership operators  practice

---
+++ demonstrate-membership-operators/demonstrate_membership_operators.py
@@ -0,0 +1,9 @@
+# Program to demonstrate membership operators
+
+subjects = ["Python", "C", "Java", "AI"]
+
+print("Python" in subjects)
+print("HTML" in subjects)
+
+print("Java" not in subjects)
+print("HTML" not in subjects)
