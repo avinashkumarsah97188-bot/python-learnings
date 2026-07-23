bd3acf5dba5a00687f5f19cd3a9cb0d6344d6034
Avinash kumar Sah <avinashkumarsah97188@gmail.com>
2026-07-23 23:31:53 +0530

    odd even programming

---
+++ odd-even/program.py
@@ -0,0 +1,11 @@
+
+#program to check whether a number is odd or Even
+
+
+num=int(input("enter a number:"))
+
+if num%2==0:
+    
+    print(num,"is an even number")
+else:
+    print(num,"is an odd numbe.")
